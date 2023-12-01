a9dfde282e0584d26ed3135cfb1caa7992a3788c
lrhtony <tony07017@163.com>
2023-12-01 14:39:00 +0800

    fix: some styles

--- themes/kira/scripts/utils/image_auto_lazyload.js
+++ themes/kira/scripts/utils/image_auto_lazyload.js
@@ -5,14 +5,6 @@ function imageAutoLazyloadHelper(content) {
 		/<img.*?src="(.*?)" alt="(.*?)".*?\/?>/gi,
 		'<img data-fancybox="gallery" data-sizes="auto" data-src="$1" alt="$2" class="lazyload">'
 	);
-	str = str.replace(
-		/src="images\//g,
-		'src="https://cdn.jsdelivr.net/gh/ch1ny/ch1ny.github.io/images/'
-	);
-	str = str.replace(
-		/\.\.\/\.\.\/images\//g,
-		'https://cdn.jsdelivr.net/gh/ch1ny/ch1ny.github.io/images/'
-	);
 	return str;
 }
 
